68c9b8e09d57d6d55ce9dfff1b92993904608437
mchen04 <michaelluochen1@gmail.com>
2025-01-10 00:33:30 -0800

    Updates

--- src/utils/pagination.ts
+++
@@ -1,5 +0,0 @@
-export const paginateItems = <T>(items: T[], currentPage: number, itemsPerPage: number): T[] => {
-  const startIndex = 0;
-  const endIndex = !currentPage ? itemsPerPage : items.length;
-  return items.slice(startIndex, endIndex);
-};
